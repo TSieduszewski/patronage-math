7adf58e74accc0bdfd4481ecb755786ddef9600f
TSieduszewski <tsieduszewski@gmail.com>
2021-01-31 22:38:23 +0100

    Szkielet aplikacji bazującej na interfejsie. Na razie tylko funkcja dodawania

--- src/main/java/Adding.java
+++ src/main/java/Adding.java
@@ -1,41 +1,47 @@
-public class Adding implements Operations{
+public class Adding implements Operations {
 
     private Double result;
+    private String message = "Brak możliwości wykonania obliczeń dla podanych typów wartości";
+
+    public Adding() {
+
+    }
 
     public Adding(Double result) {
         this.result = result;
     }
 
-    public Adding(){
-
+    public Adding(String message) {
+        this.message = message;
     }
 
+
     public Adding data(double a, double b) {
-        result = a+b;
+        result = a + b;
         return new Adding(result);
     }
 
     @Override
     public Adding data(double a, double[] b) {
-        return null;
+        return new Adding(message);
     }
 
     @Override
     public Adding data(double a, double[][] b) {
-        return null;
+        return new Adding(message);
     }
 
-    public Adding data(double[] a, double[] b){
+    public Adding data(double[] a, double[] b) {
         return new Adding(result);
     }
 
-    public Adding data(double[][] a, double[][] b){
+    public Adding data(double[][] a, double[][] b) {
         return new Adding(result);
     }
 
     @Override
     public Adding data(double[][] a, double[] b) {
-        return null;
+        return new Adding(message);
     }
 
     @Override
